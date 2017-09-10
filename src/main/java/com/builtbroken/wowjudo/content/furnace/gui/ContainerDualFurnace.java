f2ead493d2e9f592591eee2816d58dfde4f98c2f
Robert Seifert <rseifert.phone@gmail.com>
2017-09-10 02:49:42 -0400

    Implemented container slot transfer for dual furnace

--- src/main/java/com/builtbroken/wowjudo/content/furnace/gui/ContainerDualFurnace.java
+++ src/main/java/com/builtbroken/wowjudo/content/furnace/gui/ContainerDualFurnace.java
@@ -5,6 +5,9 @@ import com.builtbroken.mc.prefab.gui.slot.SlotOutput;
 import com.builtbroken.wowjudo.content.furnace.TileDualFurnace;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
+import net.minecraft.tileentity.TileEntityFurnace;
 
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
@@ -31,5 +34,88 @@ public class ContainerDualFurnace extends ContainerBase<TileDualFurnace>
 
         //Inventory
         addPlayerInventory(player);
+    }
+
+    @Override
+    public ItemStack transferStackInSlot(EntityPlayer player, int slotIndex)
+    {
+        ItemStack itemstack = null;
+        final Slot slot = (Slot)this.inventorySlots.get(slotIndex);
+
+        final int inventoryEnd = 6; //start point for player inventory, end + 1 for tile inventory
+        final int playerInvEnd = inventoryEnd + 27;
+        final int playerHotbarEnd = playerInvEnd + 9;
+
+        if (slot != null && slot.getHasStack())
+        {
+            ItemStack itemstack1 = slot.getStack();
+            itemstack = itemstack1.copy();
+
+            if (slotIndex == TileDualFurnace.OUTPUT_SLOT_1 || slotIndex == TileDualFurnace.OUTPUT_SLOT_2)
+            {
+                if (!this.mergeItemStack(itemstack1, inventoryEnd, playerHotbarEnd, true))
+                {
+                    return null;
+                }
+
+                slot.onSlotChange(itemstack1, itemstack);
+            }
+            //Move from player inventory to furnace
+            else if (slotIndex >= inventoryEnd)
+            {
+                //Input slots
+                if (FurnaceRecipes.smelting().getSmeltingResult(itemstack1) != null)
+                {
+                    if (!this.mergeItemStack(itemstack1, TileDualFurnace.INPUT_SLOT_1, TileDualFurnace.INPUT_SLOT_2 + 1, false))
+                    {
+                        return null;
+                    }
+                }
+                //Fuel slots
+                else if (TileEntityFurnace.isItemFuel(itemstack1))
+                {
+                    if (!this.mergeItemStack(itemstack1, TileDualFurnace.FUEL_SLOT_1, TileDualFurnace.FUEL_SLOT_2 + 1, false))
+                    {
+                        return null;
+                    }
+                }
+                //Hotbar
+                else if (slotIndex >= inventoryEnd && slotIndex < playerInvEnd)
+                {
+                    if (!this.mergeItemStack(itemstack1, playerInvEnd, playerHotbarEnd, false))
+                    {
+                        return null;
+                    }
+                }
+                //Main inventory
+                else if (slotIndex >= playerInvEnd && slotIndex < playerHotbarEnd && !this.mergeItemStack(itemstack1, inventoryEnd, playerHotbarEnd, false))
+                {
+                    return null;
+                }
+            }
+            //Fail state
+            else if (!this.mergeItemStack(itemstack1, inventoryEnd, playerHotbarEnd, false))
+            {
+                return null;
+            }
+
+            if (itemstack1.stackSize <= 0)
+            {
+                slot.putStack((ItemStack)null);
+            }
+            else
+            {
+                slot.onSlotChanged();
+            }
+
+            if (itemstack1.stackSize == itemstack.stackSize)
+            {
+                return null;
+            }
+
+            slot.onPickupFromSlot(player, itemstack1);
+        }
+
+        return itemstack;
     }
 }
