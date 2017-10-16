e9bb7002d5f4238c230ce5af8d045e2301281344
Robert WC Seifert <rseifert.phone@gmail.com>
2017-10-16 09:21:48 -0400

    More work on GUI, Added speed bar

--- src/main/java/com/builtbroken/wowjudo/stats/gui/GuiFrameStats.java
+++ src/main/java/com/builtbroken/wowjudo/stats/gui/GuiFrameStats.java
@@ -18,6 +18,7 @@ import java.awt.*;
 public class GuiFrameStats extends GuiFrame<GuiFrameStats>
 {
     GuiComponentStat healthStat;
+    GuiComponentStat speedStat;
 
     public GuiFrameStats(int id, int x, int y)
     {
@@ -34,13 +35,17 @@ public class GuiFrameStats extends GuiFrame<GuiFrameStats>
     public void initGui()
     {
         super.initGui();
-
-        healthStat = add(new GuiComponentStat(0, 0, 10, Color.RED, PacketStatSet.HEALTH));
-        healthStat.setRelativePosition(new HugXSide(this, 10, true));
-        healthStat.setLimits(0, StatHandler.HEALTH_MAX);
-        healthStat.setRelativeSize(new GuiRelativeSize(this, -20, 9).setUseHostHeight(false));
-
+        healthStat = addStat(0, 0, 10, PacketStatSet.HEALTH, Color.RED);
+        speedStat = addStat(1, 0, 20, PacketStatSet.SPEED, Color.YELLOW);
         updateValues();
+    }
+
+    protected GuiComponentStat addStat(int id, int x, int y, int packetID, Color color)
+    {
+        GuiComponentStat component = add(new GuiComponentStat(id, 0, y, color, packetID));
+        component.setRelativePosition(new HugXSide(this, x, true));
+        component.setRelativeSize(new GuiRelativeSize(this, -20, 9).setUseHostHeight(false));
+        return component;
     }
 
     public void updateValues()
@@ -48,6 +53,7 @@ public class GuiFrameStats extends GuiFrame<GuiFrameStats>
         StatEntityProperty property = StatHandler.getPropertyForEntity(player());
         if (property != null)
         {
+            healthStat.setLimits(0, StatHandler.HEALTH_MAX);
             healthStat.setValue(property.getHpIncrease());
         }
     }
