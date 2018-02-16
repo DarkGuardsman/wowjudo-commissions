3383270410e0e275ccf1e324e36960eb465a1938
Robert WC Seifert <rseifert.phone@gmail.com>
2018-02-15 22:17:50 -0500

    Sync

--- src/main/java/com/builtbroken/wowjudo/content/campfire/gui/GuiCampFire.java
+++ src/main/java/com/builtbroken/wowjudo/content/campfire/gui/GuiCampFire.java
@@ -13,7 +13,7 @@ import net.minecraft.inventory.Slot;
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 3/8/2017.
  */
-public class GuiCampFire extends GuiContainerBase
+public class GuiCampFire extends GuiContainerBase<TileEntityCampfire>
 {
     private TileEntityCampfire campFire;
 
